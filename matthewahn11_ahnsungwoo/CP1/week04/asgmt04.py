e2e913b20de76fdac751c43279ac22deb82ee384
matthewahn11 <matthewahn11@gmail.com>
2025-04-03 18:59:01 +0900

    feat: add week04/asgmt04

--- matthewahn11_ahnsungwoo/CP1/week04/asgmt04.py
+++ matthewahn11_ahnsungwoo/CP1/week04/asgmt04.py
@@ -1,6 +1,12 @@
-n = int(input())
-r = 0
-for i in range(0,n+1):
     if i % 3 == 0 or i % 5 == 0:
         r += i
-print(r)
+N=int(input())
+if N == 0:
+    result = 0
+else:
+    result = ""
+
+while N > 0:
+    result = str(N%2) + result
+    N //= 2
+print(result)
