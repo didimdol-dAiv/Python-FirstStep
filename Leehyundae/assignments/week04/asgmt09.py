574d1f19752e160d2d29937c925b2affbf50ade4
Lee hyun tae <hano6752@o.cnu.ac.kr>
2025-04-04 01:49:10 +0900

    fix: edit week04/09

--- Leehyundae/assignments/week04/asgmt09.py
+++ Leehyundae/assignments/week04/asgmt09.py
@@ -1,9 +1,25 @@
-n = int(input())
-nsum1 = 0
-nsum2 = 1
+def sol1():
+    n = int(input())
+    nsum1 = 0
+    nsum2 = 1
 
-for i in range(n):
-    nsum1 += nsum2
-    nsum2 = nsum1 - nsum2
+    for i in range(n):
+        nsum1 += nsum2
+        nsum2 = nsum1 - nsum2
 
-print(nsum1)
+    print(nsum1)
+
+
+def sol2():
+    N = int(input())
+    fibo = [1,1]
+
+    for _ in range(N - 2):
+        t = len(fibo)
+        result = fibo[t-2] + fibo[t-1]
+
+    print(result)
+
+
+if __name__ == "__main__":
+    sol1()
