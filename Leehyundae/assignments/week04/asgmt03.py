2cbdffd4a173230778ef268237483b1633932427
Lee hyun tae <hano6752@o.cnu.ac.kr>
2025-04-04 12:27:05 +0900

    fix: edit week04/asgmt03

--- Leehyundae/assignments/week04/asgmt03.py
+++ Leehyundae/assignments/week04/asgmt03.py
@@ -1,9 +1,9 @@
-N = int(input())
-Nsum = 0
+n = int(input())
+n_sum = 0
 
-for i in range(1,N+1):
+for i in range(1, n+1):
     if i % 3 == 0 or i % 5 == 0:
-        Nsum += i
+        n_sum += i
 
-print(Nsum)
+print(n_sum)
 
