2ef6158afcc2caf6ce5f1980605b75e070205134
Lee hyun tae <hano6752@o.cnu.ac.kr>
2025-04-15 15:04:51 +0900

    fix: edit week06/asgmt06

--- Leehyundae/assignments/week06/asgmt06.py
+++ Leehyundae/assignments/week06/asgmt06.py
@@ -1,4 +1,5 @@
 #Standardization
 def standardization(a):
-    sum_ = sum([(x-(sum(a) / len(a)))**2 for x in a])
-    return [(i-(sum(a) / len(a)))/(sum_ / len(a))**0.5 for i in a]
+    mean = sum(a) / len(a)
+    sum_ = sum([(x-mean)**2 for x in a])
+    return [(i-mean)/(sum_ / len(a))**0.5 for i in a]
